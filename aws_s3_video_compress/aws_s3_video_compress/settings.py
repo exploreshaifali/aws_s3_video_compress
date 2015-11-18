4a47cabba68b95c4a8a0c4a04a3a043c195d9ef3
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-11-19 03:02:50 +0530

    database settings

--- aws_s3_video_compress/aws_s3_video_compress/settings.py
+++ aws_s3_video_compress/aws_s3_video_compress/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'aws_s3_video_compress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'video_compressdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
