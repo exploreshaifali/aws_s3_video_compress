2647a01dec74e1ad3fc567b4368b5d370d3da207
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-11-19 03:20:52 +0530

    model for video designed

--- aws_s3_video_compress/video_compress/models.py
+++ aws_s3_video_compress/video_compress/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Video(models.Model):
+    video = models.FileField(upload_to='zaya-videos')
+    title = models.CharField(max_length=200, unique=True)
+    time = models.DateTimeField()
+    compressed_video = models.FileField(upload_to='zaya-videos', blank=True)
+
+    def save(self, *args, **kwargs):
+        self.time = datetime.now()
+        super(Video, self).save(*args, **kwargs)
