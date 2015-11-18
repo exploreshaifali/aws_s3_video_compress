da8eb7129ba946c33bb49d9cac334150be1b95d7
Shaifali Agrawal <agrawalshaifali09@gmail.com>
2015-11-19 03:38:11 +0530

    flage set to show success message

--- aws_s3_video_compress/video_compress/views.py
+++ aws_s3_video_compress/video_compress/views.py
@@ -5,6 +5,8 @@ from video_compress.models import Video
 # Create your views here.
 
 def add_video(request):
+    #set a flag to keep track is form is submitted, to show a success message
+    video_successfully_uploaded = None
     # if request is post, its form submission time :)
     if request.method == "POST":
         print("inside post request")
@@ -16,6 +18,8 @@ def add_video(request):
             # save video on s3
             print("Now actually saving data.")
             form_data = form.save()
+            #make the success message True
+            video_successfully_uploaded = True
 
         else:
             print("form is not valid")
@@ -24,4 +28,5 @@ def add_video(request):
         form = VideoForm()
     # get list of all videos saved in s3
     all_videos = Video.objects.order_by('-id').exclude(compressed_video='')
-    return render(request, 'video_compress/add_video.html', {'form': form, 'video_list': all_videos})
+    return render(request, 'video_compress/add_video.html', {'form': form, 'video_list': all_videos,
+                  'video_successfully_uploaded': video_successfully_uploaded})
