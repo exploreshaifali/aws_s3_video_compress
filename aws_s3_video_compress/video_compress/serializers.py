02ef76b88e0f98a56881ef27eb977f6b2cb9d420
Shaifali Agrawal <shaifali@zaya.in>
2016-01-08 02:16:01 +0530

    model serializer for Video added

--- aws_s3_video_compress/video_compress/serializers.py
+++ aws_s3_video_compress/video_compress/serializers.py
@@ -1,25 +1,8 @@
 from rest_framework import serializers
 
-class VideoSerializer(serializers.Serializer):
-	pk = serializers.IntegerField(read_only=True)
-	video = serializers.FileField(max_length=None, allow_empty_file=False, use_url=UPLOADED_FILES_USE_URL)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    time = serializers.DateTimeField(format=None, input_formats=None)
-    compressed_video = serializers.FileField(max_length=None, allow_empty_file=False, use_url=UPLOADED_FILES_USE_URL)
+from video_compress.models import Video
 
-    def create(self, validated_data):
-        """
-        Create and return a new `Video` instance, given the validated data.
-        """
-        return Video.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Video` instance, given the validated data.
-        """
-        instance.video = validated_data.get('video', instance.video)
-        instance.title = validated_data.get('title', instance.title)
-        instance.time = validated_data.get('time', instance.time)
-        instance.compressed_video = validated_data.get('compressed_video', instance.compressed_video)
-        instance.save()
-        return instance
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ('id', 'video', 'title', 'time', 'compressed_video')
