b09f2f0c1ec52865066b6c55e766d47b77d26634
Shaifali Agrawal <shaifali@zaya.in>
2016-01-08 17:13:33 +0530

    url to use classed based views

--- aws_s3_video_compress/video_compress/urls.py
+++ aws_s3_video_compress/video_compress/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
 
+from video_compress import views
+
 urlpatterns = [
-    url('', 'video_compress.views.add_video', name='add_video'),
+    # url('', 'video_compress.views.add_video', name='add_video'),
+    url('', views.VideoList.as_view(), name='add_video'),
 ]
+
